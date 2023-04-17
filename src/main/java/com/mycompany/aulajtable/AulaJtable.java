b99350bba36d04bf356115305c70494b34f750cf
Érico Borgonove <borgonove@gmail.com>
2023-04-17 19:02:50 -0400

    first commit

---
+++ src/main/java/com/mycompany/aulajtable/AulaJtable.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aulajtable;
+
+/**
+ *
+ * @author erico
+ */
+public class AulaJtable {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
